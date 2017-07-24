1c3b0293d08333b1faeada0fc8af2261822f1395
“piotrcyra81 <cykorsport@hotmail.com>
2017-07-25 00:30:49 +0200

    Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,28 @@
 package com.kodilla;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Calculator {
+class Calculator
+{
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
+
+class Application
+{
+    public static void main(String[] args) {
+
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        int result = simpleCalculator.addAToB(5, 5);
+
+        System.out.println(result);
+
+    }
 }
