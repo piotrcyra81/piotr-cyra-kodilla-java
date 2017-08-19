7dd8e6179c81388fd5ac8ff87fb1341ce801777b
piotrcyra <piotr.cyra@gmail.com>
2017-08-19 19:50:39 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int resultAddAToB = calculator.addAToB(5, 5);
+        int resultSubstractAFromB = calculator.substractAFromB(5, 5);
+
+        System.out.println("Result add A to B = " + resultAddAToB);
+        System.out.println("Result substract A from B = " + resultSubstractAFromB);
+
+        if (resultAddAToB==10){
+            System.out.println("test resultAddAToB OK");
+        } else {
+            System.out.println("resultAddAToB Error!");
+        }
+
+        if (resultSubstractAFromB==0){
+            System.out.println("test resultSubstractAFromB OK");
+        } else {
+            System.out.println("resultSubstractAFromB Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
