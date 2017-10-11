b534d0ef9c5e83c7c37f341143fd819d4c1a5602
piotrcyra <piotr.cyra@gmail.com>
2017-10-11 12:49:00 +0200

    Prototype 11.10

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -10,15 +10,17 @@ public class LibraryTestSuite {
     public void createLibrary() {
 
         Library library = new Library("myLibrary");
+
         Book book1 = new Book("title1", "author1", LocalDate.of(2000, 1, 1));
-        library.getBooks().add(book1);
         Book book2 = new Book("title2", "author2", LocalDate.of(2000, 2, 2));
-        library.getBooks().add(book2);
         Book book3 = new Book("title3", "author3", LocalDate.of(2000, 3, 3));
-        library.getBooks().add(book3);
         Book book4 = new Book("title4", "author4", LocalDate.of(2000, 4, 4));
-        library.getBooks().add(book4);
         Book book5 = new Book("title5", "author5", LocalDate.of(2000, 5, 5));
+
+        library.getBooks().add(book1);
+        library.getBooks().add(book2);
+        library.getBooks().add(book3);
+        library.getBooks().add(book4);
         library.getBooks().add(book5);
 
         Library clonedLibrary = null;
